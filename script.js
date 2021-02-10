2f659246634fb648df03df8ac958394950b3d178
Carlos <cadudrop@hotmail.com>
2021-02-10 20:32:16 -0300

    mudar botões

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-function pintar() {
-    var opt = document.querySelector('input[name="cor"]:checked').value;
-    var img = window.document.getElementById('imgm')
+var opt = document.querySelectorAll(".btn"); 
+var img = window.document.getElementById('imgm')
 
-    if(opt == 1){
-        img.src = 'img/bmwcianoL.png'
-    } else if(opt == 2){
-        img.src = 'img/bmwvermelhaL.png'
-    }else if(opt == 3){
-        img.src = 'img/bmwverdeL.png'
-    }else if(opt == 4){
-        img.src = 'img/bmwamarelaL.png'
-    }else if(opt == 5){
-        img.src =  'img/bmwlaranjaL.png'
-    }else if(opt == 6){
-        img.src = 'img/bmwrosaL.png'
-    } else {
-        img.src = 'img/carro-bmw.jpg'
-    }
-    
-    //tentei com switch mas não funcionou
+//change car image
+for(let i=0; i<opt.length;i++){
+    opt[i].addEventListener("click", function(){
+        img.src = this.getAttribute('data-color');
+    });
 }
