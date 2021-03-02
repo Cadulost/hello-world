062fc16d00c8bcd17940ee6e8b9727cf7247283a
Carlos <cadudrop@hotmail.com>
2021-03-02 11:47:58 -0300

    Adiciona situação

--- IMC/script.js
+++ IMC/script.js
@@ -2,13 +2,27 @@ function calcular(){
     let peso = document.querySelector("#mass").value;
     let altura = document.querySelector("#height").value;
     
-    if(peso.length === 0 || altura.length === 0){
-        alert('Faltam dados!');
+    if(peso.length === 0 || altura.length === 0 || peso == 0){
+        alert('Dados incorretos!');
     }else{
         let res = document.querySelector('.res');
         let IMC = Number(peso)/ (Number(altura) * Number(altura));
 
-        res.innerHTML = `Seu IMC é ${IMC.toFixed(2)}!`;
+        res.innerHTML = `<p>Seu IMC é ${IMC.toFixed(2)}!</p>`;
+
+        if(IMC < 18.5){
+            res.innerHTML += "<p style='margin-top:5px;'>Situação: Abaixo do peso</p>";
+        } else if(IMC < 25){
+            res.innerHTML += "<p style='margin-top:5px;'>Situação: Peso ideal</p>";
+        } else if(IMC < 30){
+            res.innerHTML += "<p style='margin-top:5px;'>Situação: Sobrepeso</p>";
+        } else if(IMC < 35){
+            res.innerHTML += "<p style='margin-top:5px;'>Situação: Obesidade grau I</p>";
+        } else if(IMC < 40){
+            res.innerHTML += "<p style='margin-top:5px;'>Situação: Obesidade grau II</p>";
+        }else{
+            res.innerHTML += "<p style='margin-top:5px;'>Situação: Obesidade grau III</p>";
+        }
     }
 }
 
